1a901b1f3159c21a8bc7fc4dce008950f15bffd3
Vagner Pavani <vagner.alves.pavani@gmail.com>
2020-07-13 22:21:42 -0300

    getting the login form value

--- Audicyan-App/src/app/login/login.page.ts
+++ Audicyan-App/src/app/login/login.page.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+
+  
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+    selector: 'app-login',
+  	templateUrl: './login.page.html',
+  	styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  	login: FormGroup;
 
-  constructor() { }
+  	constructor(public formBuilder: FormBuilder) {
+		this.login = this.formBuilder.group({
+			email: [null, [Validators.email, Validators.required]],
+			password: [null, [Validators.minLength(6), Validators.required]],
+	  });
+ 	}	
+	
+  
+  
 
-  ngOnInit() {
-  }
+ 	ngOnInit() {}
 
+  	onSubmit(){
+    	console.log(this.login.value)
+  	}
+
+  
+  
+  
 }
